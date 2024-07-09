fe0486efd3938ae6853afa9e98a1edd814a0744d
SeanoChang <chang755@purdue.edu>
2024-07-09 14:43:55 +0800

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  root: true,
+  extends: [
+    'next',
+    'eslint:recommended',
+    'prettier',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+      pragma: 'React',
+      pragmaFrag: 'React.Fragment',
+    },
+  },
+};
